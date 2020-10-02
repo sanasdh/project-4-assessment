2449247f65a747b795e88657bc16b95b27ab1a8a
sanasdh <sanasdh@gmail.com>
2020-10-02 15:17:14 -0400

    Updates

---
+++ precache-manifest.afc39abf91267b8e2768bda44a4feabc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8559f04af4aa68dc0210f59f48c1a900",
+    "url": "/sanasdh/project-4-assessment.git/index.html"
+  },
+  {
+    "revision": "fe903b3bfa66d2ed320e",
+    "url": "/sanasdh/project-4-assessment.git/static/css/main.48136b3c.chunk.css"
+  },
+  {
+    "revision": "e9bec669e0a3078f90a2",
+    "url": "/sanasdh/project-4-assessment.git/static/js/2.60ce7510.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sanasdh/project-4-assessment.git/static/js/2.60ce7510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe903b3bfa66d2ed320e",
+    "url": "/sanasdh/project-4-assessment.git/static/js/main.53965594.chunk.js"
+  },
+  {
+    "revision": "00d5508387eb5f3058de",
+    "url": "/sanasdh/project-4-assessment.git/static/js/runtime-main.d9cf4825.js"
+  }
+]);
